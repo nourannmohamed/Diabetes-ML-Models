a0c8ceadbe6b62a7b41c02736d7b29b7a95718ef
Omar Tamer <omardaihom@gmail.com>
2025-12-05 10:29:41 +0200

    uodate

--- Datamining_Phase2/KNN.py
+++ Datamining_Phase2/KNN.py
@@ -110,7 +110,7 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 # =============================
-# FIND BEST K (1 → 100)
+# FIND BEST K (1 → 151e)
 # =============================
 accuracies = {}
 
